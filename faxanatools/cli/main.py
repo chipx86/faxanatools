3718c9888d629fd14a9f5f83123f8c3cecbd2375
Christian Hammond <christian@beanbaginc.com>
2025-09-06 01:24:48 -0700

    Switch the faxanatoolsv command to use subcommands.
    Dumping of ROM information is now done with a `faxanatools dump`
    command.
    
    Patching is done with a `faxanatools patch` command.
    
    This will make it easier to integrate additional features into this CLI.

--- faxanatools/cli/main.py
+++ faxanatools/cli/main.py
@@ -9,7 +9,6 @@ import argparse
 import json
 import os
 import sys
-from typing import TYPE_CHECKING
 
 from faxanatools.decoder import ROMReader
 from faxanatools.deserializer import JSONDeserializer
@@ -18,43 +17,24 @@ from faxanatools.rom import FaxanaduROM
 from faxanatools.serializer import JSONSerializer
 
 
-def main() -> None:
-    """Main entrypoint for faxanatool.
+def add_common_rom_options(
+    rom: FaxanaduROM,
+    argparser: argparse.ArgumentParser,
+) -> None:
+    """Add common options for subcommands.
 
     Args:
-        args (list of str):
-            The command line arguments.
-    """
-    rom = FaxanaduROM()
+        rom (faxanatools.rom.FaxanaduROM):
+            The Faxanadu ROM information.
 
-    argparser = argparse.ArgumentParser()
+        argparser (argparse.ArgumentParser):
+            The subcommand argument parser to add to.
+    """
     argparser.add_argument(
         'filename',
         help='Path to the ROM file.',
     )
 
-    group = argparser.add_mutually_exclusive_group()
-    group.add_argument(
-        '--dump',
-        action='store_const',
-        dest='action',
-        default='dump',
-        const='dump',
-        help='Dump state to a file.'
-    )
-    group.add_argument(
-        '--patch',
-        action='store_const',
-        dest='action',
-        const='patch',
-        help='Write a patched ROM file instead of dumping.',
-    )
-
-    argparser.add_argument(
-        '--dump-file',
-        help='Path to the dump file.',
-        required=True,
-    )
     argparser.add_argument(
         '--script-addrs-offset',
         help=(
@@ -74,9 +54,24 @@ def main() -> None:
         default=rom.max_iscript_addrs,
     )
 
-    options = argparser.parse_args(sys.argv[1:])
 
-    # Validate and normalize our options.
+def update_rom_information(
+    argparser: argparse.ArgumentParser,
+    rom: FaxanaduROM,
+    options: argparse.Namespace,
+) -> None:
+    """Update the ROM information based on the parsed arguments.
+
+    Args:
+        argparser (argparse.ArgumentParser):
+            The subcommand argument parser to add to.
+
+        rom (faxanatools.rom.FaxanaduROM):
+            The Faxanadu ROM information.
+
+        options (argparser.Namespace):
+            The parsed options.
+    """
     try:
         if options.script_addrs_offset.startswith('0x'):
             script_addrs_offset = int(options.script_addrs_offset, 16)
@@ -102,37 +97,137 @@ def main() -> None:
     rom.iscript_addrs_offset = script_addrs_offset
     rom.max_iscript_addrs = options.num_scripts
 
-    action = options.action
+
+def main() -> None:
+    """Main entrypoint for faxanatool.
+
+    Args:
+        args (list of str):
+            The command line arguments.
+    """
+    rom = FaxanaduROM()
+
+    argparser = argparse.ArgumentParser()
+    subparsers = argparser.add_subparsers(dest='action',
+                                          required=True)
+
+    # Create the "dump" subcommand.
+    subparser = subparsers.add_parser('dump')
+    subparser.add_argument(
+        '--dump-file',
+        help='Path to the dump file.',
+        required=True,
+    )
+    add_common_rom_options(rom, subparser)
+
+    # Create the "parse-password" subcommand.
+    subparser = subparsers.add_parser('parse-password')
+    subparser.add_argument(
+        '--password',
+        help='The password to parse',
+        required=True,
+    )
+
+    # Create the "patch" subcommnad.
+    subparser = subparsers.add_parser('patch')
+    subparser.add_argument(
+        '--dump-file',
+        help='Path to the dump file.',
+        required=True,
+    )
+    add_common_rom_options(rom, subparser)
+
+    # Parse the command line options and run the command.
+    options = argparser.parse_args(sys.argv[1:])
+
+    COMMANDS[options.action](argparser=argparser,
+                             options=options,
+                             rom=rom)
+
+
+def handle_dump_command(
+    argparser: argparse.ArgumentParser,
+    options: argparse.Namespace,
+    rom: FaxanaduROM,
+) -> None:
+    """Handle the "dump" command.
+
+    Args:
+        argparser (argparse.ArgumentParser):
+            The subcommand argument parser to add to.
+
+        options (argparser.Namespace):
+            The parsed options.
+
+        rom (faxanatools.rom.FaxanaduROM):
+            The Faxanadu ROM information.
+    """
     dump_file = options.dump_file
 
-    if action == 'dump':
-        # Read the ROM.
-        rom_reader = ROMReader(rom=rom)
-        rom_reader.read(options.filename)
+    update_rom_information(argparser=argparser,
+                           options=options,
+                           rom=rom)
 
-        # Serialize to JSON.
-        serializer = JSONSerializer(rom=rom)
+    # Read the ROM.
+    rom_reader = ROMReader(rom=rom)
+    rom_reader.read(options.filename)
 
-        with open(dump_file, 'w') as fp:
-            json.dump(serializer.serialize(),
-                      fp,
-                      sort_keys=True,
-                      indent=True)
-    elif action == 'patch':
-        if not os.path.exists(dump_file):
-            argparser.error(f'The dump file ({dump_file}) was not found.')
+    # Serialize to JSON.
+    serializer = JSONSerializer(rom=rom)
 
-        # Patch the ROM.
-        with open(dump_file, 'r') as fp:
-            rom = (
-                JSONDeserializer(json.load(fp))
-                .deserialize()
-            )
+    with open(dump_file, 'w') as fp:
+        json.dump(serializer.serialize(),
+                  fp,
+                  sort_keys=True,
+                  indent=True)
 
-        patched_file = '%s-patched.nes' % filename.rstrip('.nes')
+    print(f'Wrote Faxanadump file to {dump_file}')
 
-        rom_patcher = ROMPatcher(rom=rom)
-        rom_patcher.patch(filename,
-                          patched_filename=patched_file)
 
-        print(f'Wrote patched file to {patched_file}')
+def handle_patch_command(
+    argparser: argparse.ArgumentParser,
+    options: argparse.Namespace,
+    rom: FaxanaduROM,
+) -> None:
+    """Handle the "patch" command.
+
+    Args:
+        argparser (argparse.ArgumentParser):
+            The subcommand argument parser to add to.
+
+        options (argparser.Namespace):
+            The parsed options.
+
+        rom (faxanatools.rom.FaxanaduROM):
+            The Faxanadu ROM information.
+    """
+    dump_file = options.dump_file
+    filename = options.filename
+
+    if not os.path.exists(dump_file):
+        argparser.error(f'The dump file ({dump_file}) was not found.')
+
+    update_rom_information(argparser=argparser,
+                           options=options,
+                           rom=rom)
+
+    # Patch the ROM.
+    with open(dump_file, 'r') as fp:
+        rom = (
+            JSONDeserializer(json.load(fp))
+            .deserialize()
+        )
+
+    patched_file = '%s-patched.nes' % filename.rstrip('.nes')
+
+    rom_patcher = ROMPatcher(rom=rom)
+    rom_patcher.patch(filename,
+                      patched_filename=patched_file)
+
+    print(f'Wrote patched file to {patched_file}')
+
+
+COMMANDS = {
+    'dump': handle_dump_command,
+    'patch': handle_patch_command,
+}
